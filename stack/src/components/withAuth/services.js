d7509c3ba4d13d57a04497f8f11f849e1476f0bc
Cai Martin <cmartin.professional@yahoo.com>
2019-06-26 13:55:07 -0400

    ADDed ADD PAGE

--- stack/src/components/withAuth/services.js
+++ stack/src/components/withAuth/services.js
@@ -27,7 +27,6 @@ export const handleLogin = async ({ email, password }) => {
   return false;
 };
 export const handleRegister = async ({ email, password, fName, lName }) => {
-  console.log(email + " " + password + " " + fName + " " + lName);
   await axios
     .post("https://bw-business-card-org-be-raine.herokuapp.com/api/register", {
       email: email,
@@ -36,6 +35,43 @@ export const handleRegister = async ({ email, password, fName, lName }) => {
       lastName: lName,
       subscription: 0
     })
+    .then(res => {
+      return true;
+    })
+    .catch(res => {
+      alert(res.message);
+      return false;
+    });
+};
+export const handleAddCard = async ({
+  businessName,
+  address,
+  phone,
+  notes
+}) => {
+  console.log(businessName, address, phone, notes);
+  let user = getUser();
+  await axios
+    .post(
+      "https://bw-business-card-org-be-raine.herokuapp.com/api/cards",
+
+      {
+        businessName: businessName,
+        address: address,
+        phone: phone,
+        logoPic: null,
+        additionalPic: null,
+        phone2: null,
+        blurb: null,
+        hours: null,
+        email: null,
+        website: null,
+        notes: notes
+      },
+      {
+        headers: { Authorization: `${user.token}` }
+      }
+    )
     .then(res => {
       return true;
     })
